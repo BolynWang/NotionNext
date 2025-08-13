46a8eb6a1823ba48cb11566c49f6a34f16fd10cc
JiazanWang <1710998763@qq.com>
2025-08-13 22:33:09 +0800

    修复一个人github连接

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -55,7 +55,7 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https:/github.com/BolynWang',
+  HEO_INFO_CARD_URL2: 'https://github.com/BolynWang',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
   HEO_INFO_CARD_URL3: 'https://wbolyn.com',
   HEO_INFO_CARD_TEXT3: '了解更多',
